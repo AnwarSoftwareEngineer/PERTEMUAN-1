896b9e5a602e45659730465d2f2b18e40d5c8555
Aisya <aisyanwarputri@gmail.com>
2022-03-17 23:14:22 +0700

    Percobaan 3 whitespaces, 4 block dan komentar, 5 Varaibel

---
+++ src/main/kotlin/4_BlockdanKomentar.kt
@@ -0,0 +1,10 @@
+fun main() {
+    println("Hello, Ini program saya di kotlin")
+    //contoh komentar satu baris
+    /*
+    contoh komentar multi baris
+     */
+    /**
+     * kotlin doc --> baris ini untuk mendokumentasikan kode program
+     */
+}
---
+++ src/main/kotlin/5_Variabel.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var dynamicVar = 2
+    println("contoh dynamic variabel = $dynamicVar")
+
+    var staticVar: Char = 'A'
+    println("contoh dynamic variabel = $staticVar")
+}

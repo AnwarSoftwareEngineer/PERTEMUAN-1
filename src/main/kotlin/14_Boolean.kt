2ab1031ff42cf02c4cd6d3b5d407ba81879fc580
Aisya <aisyanwarputri@gmail.com>
2022-03-17 23:34:37 +0700

    Percobaan 14 Boolean

--- src/main/kotlin/14_Boolean.kt
+++ src/main/kotlin/14_Boolean.kt
@@ -2,5 +2,9 @@ fun main() {
     var varBool : Boolean = true
     if(varBool)
         println("varBool bernilai " + varBool)
+    //Tambahkan kode berikut dalam program dan perhatikan hasilnya:
+//    var varInt : Int = 1if(varInt)
+//        println("varInt bernilai " + varInt)
+
 
 }

29115544f19a5ae756de874a3916061a02bd56d7
Aisya <aisyanwarputri@gmail.com>
2022-03-17 23:54:35 +0700

    Percobaan 20 Perintah Masukan di Kotlin

---
+++ src/main/kotlin/20_Perintah_Masukan_di_Kotlin.kt
@@ -0,0 +1,10 @@
+fun main() {
+    //Parrot program
+    println("Hi I'm Aisya, the virtual parrot, and i love to repeat!");
+    println("Type something in: ");
+    var input: String
+    input = readLine()!!
+    var output: String
+    output = input + ", " + input + "!"
+    println(output)
+}

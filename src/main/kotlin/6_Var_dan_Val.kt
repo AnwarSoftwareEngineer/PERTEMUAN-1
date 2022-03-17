69cf4e0df2982ea4fc3da17e50f8a8df9e780875
Aisya <aisyanwarputri@gmail.com>
2022-03-17 23:16:16 +0700

    Percobaan 6 Var dan Val

---
+++ src/main/kotlin/6_Var_dan_Val.kt
@@ -0,0 +1,18 @@
+fun main() {
+    //contoh kode menggunakan var
+    var varVariabel: Int
+    varVariabel = 56 //assigning first value to varVariabel
+    println("Pemberian nilai pertama pada varVariabel = "+varVariabel)
+    varVariabel = 78 //assigning second value to varVariabel
+    println("Pemberian nilai kedua pada varVariabel = "+varVariabel)
+
+    //contoh kode menggunakan val
+    val phi = 3.14
+    println("Phi = " + phi)
+    //phi = 2.5
+    /*
+    Ketika kita merubah nilai variabel val phi untuk kedua kalinya
+    maka akan terdapat pesan kesalahan agar kita menggunakan var variabel.
+     */
+
+}

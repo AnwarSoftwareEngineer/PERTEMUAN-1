6184187565daf8e588302afded9059ac60d50918
Aisya <aisyanwarputri@gmail.com>
2022-03-17 23:41:02 +0700

    Percobaan 15 String

--- src/main/kotlin/15_String.kt
+++ src/main/kotlin/15_String.kt
@@ -4,10 +4,21 @@ fun main() {
         print(chr)
     }
     print('\n')
+    println("==============================================================")
 
     //Kita bisa menggabungkan String dengan tipe data lain dengan menggunakan operator +,
     //sehingga tipe data yang digabung otomatis akan melebur kedalam String.
     val s = "abc" + 1
     println(s + "def")
+    println("==============================================================")
+
+    //Untuk dapat menyimpan data String yang memuat keywords Kotlin bisa dengan menggunakan
+    //raw String yang dipisahkan dengan triple quotes (""").
+    //Contoh kode program raw String:
+    var text = """
+ for (c in "foo")
+ print(c)
+ """
+    println(text)
 
 }

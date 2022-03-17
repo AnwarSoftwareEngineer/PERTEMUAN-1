42d40b5e8c8039a6e1f2b07309296d00b1f2a4ec
Aisya <aisyanwarputri@gmail.com>
2022-03-17 23:42:38 +0700

    Percobaan 15 String

--- src/main/kotlin/15_String.kt
+++ src/main/kotlin/15_String.kt
@@ -15,10 +15,21 @@ fun main() {
     //Untuk dapat menyimpan data String yang memuat keywords Kotlin bisa dengan menggunakan
     //raw String yang dipisahkan dengan triple quotes (""").
     //Contoh kode program raw String:
-    var text = """
+    var text1 = """
  for (c in "foo")
  print(c)
  """
-    println(text)
+    println(text1)
+
+    println("==============================================================")
+    //Kita juga dapat menghapus spasi yang tidak diperlukan dalam raw String seperti contoh kode
+    //program berikut:
+    var text2 = """
+ |Tell me and I forget.
+ |Teach me and I remember.
+ |Involve me and I learn.
+ |(Benjamin Franklin)
+ """.trimMargin()
+    println(text2)
 
 }

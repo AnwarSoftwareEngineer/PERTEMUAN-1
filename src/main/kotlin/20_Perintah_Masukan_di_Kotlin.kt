4ccc0c048eb95f85edc0d8c82a018ec7a53c5304
Aisya <aisyanwarputri@gmail.com>
2022-03-17 23:56:53 +0700

    Percobaan 20 Perintah Masukan di Kotlin

--- src/main/kotlin/20_Perintah_Masukan_di_Kotlin.kt
+++ src/main/kotlin/20_Perintah_Masukan_di_Kotlin.kt
@@ -7,4 +7,20 @@ fun main() {
     var output: String
     output = input + ", " + input + "!"
     println(output)
+
+    println("==============================================================")
+    //Untuk merubah nilai String yang dikembalikan oleh fungsi readLine()!! kedalam nilai yang
+    //diinginkan bisa dilakukan dengan menggunakan konversi secara eksplisit seperti contoh program
+    //berikut:
+    //Doubler program parsing
+    println("Enter a number and I'll double it: ")
+    val input2 = readLine()!!
+    var a = input2.toInt() // eventually Double
+    a = a * 2
+    println(a)
+    println("Enter a number, and I'll square it:")
+    val number = readLine()!!.toInt()
+    val square = number * number
+    println("Result: " + square)
+
 }
